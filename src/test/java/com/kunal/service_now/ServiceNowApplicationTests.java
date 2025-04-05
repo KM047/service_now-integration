fe0ea0c66a1249569658edfd3e5b830eed1ae7e0
Kunal Mandave <mandavekunal@gmail.com>
2025-04-05 15:09:15 +0530

    Initial commit

---
+++ src/test/java/com/kunal/service_now/ServiceNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.service_now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
